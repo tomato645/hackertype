a61bffec2b99b5388111551ca2b6225424b6f6de
tomato645 <rajiohousou@gmail.com>
2022-02-11 00:16:44 +0900

    add slider.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var sentens = "let len = sentens.length-sentens.length;<br/>let max__len = sentens.length;<br/>let show_sentens = <br/>let len = sentens.length-sentens.length;<br/>let max__len = sentens.length;<br/>let show_sentens = <br/> function type(){<br/>  if (len < max__len) {show_sentens = show_sentens + sentens.charAt(len);<br/>document.getElementById(hackertyper).innerHTML = show_sentens;<br/>len++<br/>}<br/>document.addEventListener('keypress', type);"
 // var sentens = "aaaaae"
 
-// TODO: resetで文章も？
 // TODO: 乱数偏りなくしたい
 
 let len = sentens.length-sentens.length;
@@ -10,19 +9,34 @@ let show_sentens = ""
 
 // ランダムな文字数をsentensから表示
 function type(){
+  // 文章が終わったか
   if (len < max__len) {
-    var repeat = Math.floor(Math.random() * (4 - 1) + 1);
-    console.log(repeat);
-
+    // 乱数が満たされるまで文字の表示を繰り返す。
+    random();
     for (let i = 0; i <= repeat; i++){
-      console.log("doing");
       show_sentens = show_sentens + sentens.charAt(len);
       document.getElementById("hackertyper").innerHTML = show_sentens;
       len++
     }
   }else{
+    // 文章が終わったら
     granted();
   }
+}
+
+// 変数宣言
+let repeat = 0;
+var max = document.getElementById("speed");
+var min = 1;
+
+// 数値を生成
+function random() {
+  // speedを取得
+  max = document.getElementById("speed");
+  // speedを出力
+  document.getElementById("Speed_num").innerHTML = "("+ max.value +")";
+  // 乱数を生成
+  repeat = Math.floor(Math.random() * (max.value - min) + min);
 }
 
 // 成功表示
@@ -43,7 +57,6 @@ var num = 0;
 document.addEventListener('keydown', event => {
   if (event.ctrlKey) {
     num++
-    console.log("ctrlkey"+num);
 
     if (num == 3) {
       granted();
@@ -57,7 +70,6 @@ document.addEventListener('keydown', event => {
 });
 
 function change_font(font) {
-  console.log(font);
   document.documentElement.style.setProperty("--font-family", font);
 }
 
